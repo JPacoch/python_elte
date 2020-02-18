da3b5ef676a49274e30e696aa8dc60d49495e592
JPacoch <pacochp@gmail.com>
2020-02-18 21:20:51 +0100

    cos tam porobione lol

--- plik_testowy.py
+++ plik_testowy.py
@@ -1,7 +1,26 @@
-a = "Kiedyś ktoś to przeczyta i będzie śmiesznie"
+c = "Kiedyś ktoś to przeczyta i będzie śmiesznie"
 b = "Twój stary"
 
 
 if b == "Twój stary":
-    print(a)
+    print(c)
 else: print(b)
+
+for i in range(-3,24,4):  #wybór co czwartej liczby z przedziału (-3;24)
+    print(i)
+
+cities = ["Pecs", "Budapest", "Debrecen", "Sopron", "Szolnok", "Miszkolc", "Eger"]
+
+for i, city in enumerate(cities):  #wybór 4 pierwszych miast z listy za pomocą pętli for które nie są stolicą
+    if city == "Budapest":
+        continue
+    if i == 4:
+        break
+    print(i)
+    print("{} spełnia wymagania".format(city))
+
+
+while True:  #pętla do obliczenia sześcianu liczby wybranej przez użytkownika
+    x = input("Wpisz liczbę, aby uzyskać jej sześcian. ")
+    a = int(x)
+    print(a*a*a)
