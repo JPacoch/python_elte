4489d7405f81de7e5c8b32b33e0aed91ca5b286e
JPacoch <pacochp@gmail.com>
2020-02-27 23:46:03 +0100

    no i teraz sobie konczy prace a nie leci

--- plik_testowy.py
+++ plik_testowy.py
@@ -22,11 +22,16 @@ for i, city in enumerate(cities):  #wybór 4 pierwszych miast z listy za pomocą
 print(2.2 * 3.0 == 6.6) ## co tu sie odpierdala XDXDXD
 print(float(2.2 * 3.0))
 
-
-print(3.3 * 2.0 == 6.6)
+print(3.3 * 2.0 == 6.6) ## no a tu juz jest git
 print(float(3.3 * 2.0))
 
-while True:  ## kalkulator w pętli while [tylko, że się nie konczy jeszcze]
-    x = (input("Wybierz liczbę by uzyskać jej sześcian: "))
-    a = int(x)
-    print(a * a * a)
+while True:  ## kalkulator w pętli while [który już się kończy po jednej operacji i wydaje błąd]
+        try:
+            x = (input("Wybierz liczbę by uzyskać jej sześcian: "))
+            a = float(x)
+            print(a * a * a)
+            break
+        except ValueError:
+            print("Oops! Kalkulator nie przyjmuje tekstu i/lub dat.")
+            break
+
