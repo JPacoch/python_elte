3f0487059994b8681e155f29620242f9afd5be1b
JPacoch <pacochp@gmail.com>
2021-01-07 18:09:50 +0100

    commit

--- viewsBot.py
+++ viewsBot.py
@@ -1,9 +1,9 @@
 import time
 from selenium import webdriver
 
-refreshInterval = 1
-viewsCount = 20
-webpageLink = 'https://twitter.com/JakubPacocha'
+refreshInterval = 16
+viewsCount = 40
+webpageLink = 'https://www.tiktok.com/@researchfund/video/6914260324837231878?lang=pl-PL'
 
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get(webpageLink)
